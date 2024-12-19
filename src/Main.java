e7cb9dcb988a3b233acbe32d26695b108688970d
VladTca <votsats@gmail.com>
2024-12-19 09:28:46 +0100

    Update Main.java to print formatted building info
    Replaced the generic "Hello, World!" output with a formatted string displaying address, number of floors, and occupants. Improves relevance and potential usability for real-world applications.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String homeAdress = "ул. Горького 18";
+        int stages = 7;
+        int menschnummer = 120;
+
+        System.out.println(homeAdress+", "+stages+" этажей, "+menschnummer+" человек.");
+
     }
 }
